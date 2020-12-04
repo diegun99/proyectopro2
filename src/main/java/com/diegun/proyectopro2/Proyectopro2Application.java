d0183aadb49c05edfe1d982ed771f8af8f4d47ee
diegun99 <diegun123456789@gmail.com>
2020-12-03 20:12:04 -0500

    first commit

---
+++ src/main/java/com/diegun/proyectopro2/Proyectopro2Application.java
@@ -0,0 +1,13 @@
+package com.diegun.proyectopro2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyectopro2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyectopro2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegun/proyectopro2/Proyectopro2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegun.proyectopro2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyectopro2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
